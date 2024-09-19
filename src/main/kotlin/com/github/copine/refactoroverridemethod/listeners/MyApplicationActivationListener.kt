f9fe9f6f735f207d45e151a8a4d1d1d8866d43ee
GitHub Action <action@github.com>
2024-09-19 14:46:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/copine/refactoroverridemethod/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/copine/refactoroverridemethod/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.copine.refactoroverridemethod.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
